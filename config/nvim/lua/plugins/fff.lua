8742f00d4b91f57fab93e0681d0d2efe695f3f05
Dillon Jaap <djaap@us.ci.org>
2025-10-27 11:50:34 -0600

    fix

--- config/nvim/lua/plugins/fff.lua
+++ config/nvim/lua/plugins/fff.lua
@@ -17,12 +17,12 @@ return {
 	-- This plugin initializes itself lazily.
 	lazy = false,
 	keys = {
-	-- 	{
-	-- 		"ff", -- try it if you didn't it is a banger keybinding for a picker
-	-- 		function()
-	-- 			require("fff").find_files()
-	-- 		end,
-	-- 		desc = "FFFind files",
-	-- 	},
-	-- },
+		-- 	{
+		-- 		"ff", -- try it if you didn't it is a banger keybinding for a picker
+		-- 		function()
+		-- 			require("fff").find_files()
+		-- 		end,
+		-- 		desc = "FFFind files",
+		-- 	},
+	},
 }
